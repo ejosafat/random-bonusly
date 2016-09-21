791594725460c524169db58271077cd20d4378e9
Eddy Hernandez <eddy.josafat@basekit.com>
2016-09-21 20:26:14 +0100

    Build a bonus msg

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const https = require('https');
+const spawnSync = require('child_process').spawnSync;
 
 const accessToken = require('./secrets.json').access_token;
 const apiUrl = 'bonus.ly/api/v1/';
@@ -19,9 +20,17 @@ https.get(requestUri, (res) => {
 
   res.on('end', () => {
     users = JSON.parse(output).result.map(entry => entry.username);
-    console.log(users);
+    const user = users[getRandomInt(0, users.length)];
+    const fortune = spawnSync('fortune', ['startrek']).stdout.toString();
+    let bonus = `+1 @${user} ${fortune} #why-so-serious`;
+    console.log(bonus);
   });
 }).on('error', (e) => {
   console.log(e);
 });
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
