f5011a69827522bf2615ef541171c39c5153cb81
Eddy Hernandez <eddy.josafat@basekit.com>
2016-09-22 21:41:32 +0100

    Post random bonus

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const https = require('https');
+const request = require('request');
+
 const spawnSync = require('child_process').spawnSync;
 
 const accessToken = require('./secrets.json').access_token;
@@ -24,10 +26,26 @@ https.get(requestUri, (res) => {
     const fortune = spawnSync('fortune', ['startrek']).stdout.toString();
     let bonus = `+1 @${user} ${fortune} #why-so-serious`;
     console.log(bonus);
+    postBonus(bonus);
   });
 }).on('error', (e) => {
   console.log(e);
 });
+
+function postBonus(bonus) {
+    request.post({
+        url: `https://bonus.ly/api/v1/bonuses?access_token=${accessToken}`,
+        json: {
+          reason: bonus,
+        }
+      }, (err, resp, body) => {
+        if (resp.statusCode === 200) {
+          console.log(`Remaining: ${body.result.giver.giving_balance} BK bucks`);
+        } else {
+          console.log(err);
+        }
+      });
+}
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
