6345526b13f5da90eaa234602f1323a5ce7b3f28
Eddy Hernandez <eddy.josafat@basekit.com>
2016-12-07 19:45:23 +0000

    Add list of fortune sets available to help text

--- app/help.js
+++ app/help.js
@@ -1,4 +1,5 @@
 const api = require('./bonuslyApi');
+const spawnSync = require('child_process').spawnSync;
 
 function getHelpText(paramsHelpText) {
     return Promise.all([api.getHashtags(), api.getUsers()]).then((result) => {
@@ -12,10 +13,23 @@ ${helpText}
 Available hashtags:
 ${hashtags.join(', ')}
 
+Available sets:
+${getFortuneSets()}
+
 Available users:
 ${users.join(', ')}
     `;
     return text;
+}
+
+function getFortuneSets() {
+    const cmdOutput = spawnSync('fortune', ['-f']).stderr.toString().split('\n');
+    const setRegexp = /[a-z-]+/;
+    cmdOutput.pop();
+    return cmdOutput.map(set => {
+        return setRegexp.exec(set).shift();
+    }).slice(1).join(', ');
+
 }
 
 module.exports = getHelpText;
